f5e2d49ab70f6de417e977fccaaa3549e7cf3339
Emad A. Ismael <EmadAIsmael@yahoo.com>
2021-02-22 17:15:01 +0200

    Mon. Feb., 22nd 2021 17:14

--- Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -4,7 +4,7 @@ package tictactoe
 class TicTacToe {
     val rows = 3
     val columns = 3
-    val board = Array(3) { arrayOf("O", "O", "O") }
+    val board = Array(3) { arrayOf("_", "_", "_") }
 
     fun setX(row: Int, col: Int) {
         board[row-1][col-1] = "X"
@@ -14,17 +14,24 @@ class TicTacToe {
         board[row-1][col-1] = "O"
     }
 
+    fun drawUserRep(str: String) {
+        for (r in 1..3)
+            for (c in 1..3)
+                board[r-1][c-1] = str[((r-1)*3)+c-1].toString()
+        println(this)
+    }
+
     override fun toString(): String {
-        var boardDrawing = ""     // " _ _ _\n"
+        var boardDrawing = "---------\n"
         for (r in 1..rows) {
-            // boardDrawing += "|"
+             boardDrawing += "| "
             for (c in 1..columns) {
                 boardDrawing += "${board[r - 1][c - 1]} "
-                // boardDrawing += "|"
+                 // boardDrawing += "| "
             }
-            boardDrawing += "\n"
+            boardDrawing += "|\n"
         }
-        // boardDrawing += " - - -"
+         boardDrawing += "---------\n"
 
         return boardDrawing
     }
@@ -34,8 +41,11 @@ class TicTacToe {
 fun main() {
     val tictactoe = TicTacToe()
 
-    tictactoe.setX(1, 2)
-    tictactoe.setX(2, 3)
-
-    println(tictactoe)
+//    tictactoe.setX(1, 2)
+//    tictactoe.setX(2, 3)
+//
+//    println(tictactoe)
+    print("Enter cells: ")
+    val cells = readLine()!!
+    tictactoe.drawUserRep(cells)
 }
